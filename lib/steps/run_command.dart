676d25d3a54d12ddf14bed5b7469ca284b775866
Pau Fernández <pau.fernandez@upc.edu>
2023-10-22 00:25:35 +0200

    Can set environment on RunCommand

--- lib/steps/run_command.dart
+++ lib/steps/run_command.dart
@@ -3,27 +3,36 @@ import 'dart:io';
 import 'package:installer/installer.dart';
 import 'package:installer/steps/nushell/configure_nushell.dart';
 import 'package:installer/steps/step.dart';
-import 'package:path/path.dart';
+
+class EnvVar {
+  String variable, value;
+  EnvVar({required this.variable, required this.value});
+}
 
 class RunCommand extends SinglePriorStep {
   final String cmd;
   final List<String> args;
-  RunCommand(this.cmd, this.args)
-      : super("Run command '$cmd ${args.join(" ")}");
+  final List<String> envPath;
+  RunCommand(
+    this.cmd, {
+    this.args = const [],
+    this.envPath = const [],
+  }) : super("Run command '$cmd ${args.join(" ")}'");
 
   @override
   Future run() async {
-    final envPath = Platform.environment['Path']?.split(';') ?? [];
+    final pathList = Platform.environment['Path']?.split(';') ?? [];
 
-    // Add node path for npm installs!
-    envPath.add(
-      dirname(ctx.getBinary("node")),
-    );
+    // Add directories to PATH
+    for (final dir in envPath) {
+      pathList.add(dir);
+    }
+
     final cmdPath = ctx.getBinary(cmd);
-    final env = {
-      pathVariable: envPath.join(";"),
-    };
-    final result = await Process.run(cmdPath, args, environment: env);
+
+    final result = await Process.run(cmdPath, args, environment: {
+      pathVariable: pathList.join(";"),
+    });
 
     if (result.exitCode != 0) {
       log.print("ERROR: $cmd returned ${result.exitCode}.");
